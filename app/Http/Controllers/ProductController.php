ecbc51a7498ebfe3b6fb552c1eb211481523f1ce
nazar alif <alifnazar4@gmail.com>
2025-02-05 10:14:08 +0700

    tuwaawa

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class ProductController extends Controller
+{
+  
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json([
+            'success' => true,
+            'data' => $products
+        ], 200); 
+    }
+
+    
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'price' => 'required|numeric',
+                'stock' => 'required|integer'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'status' => 400,
+                'message' => 'Bad Request: Invalid input',
+                'errors' => $e->errors()
+            ], 400); 
+        }
+
+        $product = Product::create($request->all());
+        return response()->json([
+            'success' => true,
+            'data' => $product
+        ], 201); 
+    }
+
+   
+    public function show($id)
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $product
+        ], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product not found'
+            ], 404);
+        }
+
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'price' => 'required|numeric',
+                'stock' => 'required|integer'
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Bad Request: Invalid input',
+                'status' => 400,
+                'errors' => $e->errors()
+            ], 400); 
+        }
+
+        $product->update($request->all());
+        return response()->json([
+            'success' => true,
+            'data' => $product
+        ], 200); 
+    }
+
+  
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product not found'
+            ], 404);
+        }
+
+        $product->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Product deleted successfully'
+        ], 204);
+    }
+  
+
+        public function acceptedExample()
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Request accepted, processing in progress'
+        ], 202); // Accepted
+    }
+}
